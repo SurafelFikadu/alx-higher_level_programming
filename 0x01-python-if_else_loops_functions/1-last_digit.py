4d0969bce85128f8ade18a349a8883cad32dbe3b
SurafelFikadu <surafelfikadu05@gmail.com>
2023-03-07 05:49:15 -0800

    task 0

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import random
+number = random.randit(-10000, 10000)
+
+if numbe%10 > 5:
+    print("and id greater than 5")
+elif number%10 == 0:
+    print("and is 0")
+elif number%10 < 6 and number%10 != 0:
+    print("and is less than 6 and not 0")
