43d36594da668766cef62fa7a94d5e36dcf86ff3
SurafelFikadu <surafelfikadu05@gmail.com>
2023-03-09 15:41:47 -0800

    import

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-
-    name = dir(hidden_4)
-
-    for name in names:
+    for name in sorted(dir(hidden_4)):
         if name [:2] != '__':
-            print(name)
+            print("{}".format(name))
