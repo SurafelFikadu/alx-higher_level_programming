09e04565c49be8afca971eb3e1bd8b79f7adc021
SurafelFikadu <surafelfikadu05@gmail.com>
2023-03-16 09:04:10 -0700

    sure

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    idx = 3
+    my_list = [1, 2, 3, 4, 5]
+    new_element = 9
+    if idx < 0 and idx != my_list:
+        return my_list
+    my_list[3] = new_element
