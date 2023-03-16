e4773bbe6aac389d47c1ec59c54b4d2de4508475
SurafelFikadu <surafelfikadu05@gmail.com>
2023-03-16 09:06:09 -0700

    sure

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,7 +2,9 @@
 def replace_in_list(my_list, idx, element):
     idx = 3
     my_list = [1, 2, 3, 4, 5]
+    print(my_list)
     new_element = 9
     if idx < 0 and idx != my_list:
         return my_list
     my_list[3] = new_element
+    print(my_list)
