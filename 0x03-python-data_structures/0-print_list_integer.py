bcd0960305a35aac52e4ae54b53f85a5dcd42c65
SurafelFikadu <surafelfikadu05@gmail.com>
2023-03-16 07:39:51 -0700

    lists

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     my_list = [1, 2, 3, 4, 5]
-    print_last_integer(my_list)
+
 for my_list in range(1, 6):
-    print(my_list)
+    print(print_list_integer(my_list))
 print()
