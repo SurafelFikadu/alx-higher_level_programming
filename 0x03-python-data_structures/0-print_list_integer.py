b8b0db613f4fe8ef1fd1dcd1ed0a5a5b0f4550d5
SurafelFikadu <surafelfikadu05@gmail.com>
2023-03-17 13:20:37 -0700

    task 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    return my_list
-    my_list = [1, 2, 3, 4, 5]
-
-for my_list in range(1, 6):
-    print(print_list_integer(my_list))
-print()
+    for i in my_list:
+        print('{:d}'.foramt(i))
