01e24494b1f12d375329185f0605689292be7e3c
SurafelFikadu <surafelfikadu05@gmail.com>
2023-03-18 01:35:03 -0700

    task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,5 +2,7 @@
 for i in range(100):
     if i < 10:
         print("0{}".format(i), end=", ")
-    else:
+    elif i > 9 and i != 99:
         print("{}".format(i), end=", ")
+    else:
+        print("{}".format(i))
