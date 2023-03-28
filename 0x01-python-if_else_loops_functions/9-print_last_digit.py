970274990a1500055281b94cb5ff5b004da336be
SurafelFikadu <surafelfikadu05@gmail.com>
2023-03-28 10:21:18 -0700

    task 9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if number >= 0:
-        return (number % 10)
+    if number > 0:
+        number = number % 10
     else:
-        return (abs(number) % 10) * (-1)
+        aux = number * -1
+        number = aux % 10
+    print(number, end="")
+    return number
