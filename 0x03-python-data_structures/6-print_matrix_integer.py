cfbcad30a23046b351ec6df617ed56284022b14f
SurafelFikadu <surafelfikadu05@gmail.com>
2023-04-04 12:50:59 -0700

    task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
-def pringt_matrix_integet(matrix=[[]]):
-    for i in matrix:
-        print(' '.join('{:d}'.format(j)for j in i))
+def print_matrix_integer(matrix=[[]]):
+    for i in range(len(matrix)):
+        for j in range((len(matrix[i]))):
+            if (j < (len(matrix[i])) - 1):
+                print("{:d}".format(matrix[i][j]), end=' ')
+            else:
+                print("{:d}".format(matrix[i][j]), end='')
+        print()
